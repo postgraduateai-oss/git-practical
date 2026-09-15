71318b834c5118295a95cecfa588ad66be972843
Saw Kyaw Moe Min <postgraduateai@gmail.com>
2026-09-15 13:38:20 +0630

    1 commis grade-git.py modify in local

---
+++ grade-git.py
@@ -0,0 +1,13 @@
+name=input("Enter students name: ")
+subject1=float(input("Enter first subject mark: "))
+subject2=float(input("Enter second subject mark: "))
+avg_marks=(subject1+subject2)/2
+total_marks=subject1+subject2
+if subject1 < 40 or subject2 < 40:
+    overall_status = "Failed"
+else:
+    overall_status = "Passed"
+print("Student name: ",name)
+print("Total Mark: ",total_marks)
+print("Average Mark: ",avg_marks)
+print("Result: ",overall_status)
